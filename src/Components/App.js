362237f92f2c81c418caf2e91b54ddc657283ab5
jungsehwan <jungsehwan0612@gmail.com>
2019-12-31 01:30:32 +0900

    1.React_Setup까지 작성 완료

---
+++ src/Components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Router from 'Components/Router';
+import Header from 'Components/Header';
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+      <Header />
+      <Router />
+    </>
+    )
+  }
+}
+
+export default App;
